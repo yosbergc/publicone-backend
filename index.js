40211cda58716bb1397f3da294c0ae93380f0152
Yosber Guerrero <yosbergc@gmail.com>
2024-11-03 19:45:26 -0400

    Added sequelize lib to contact db, added index

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT)
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelizeInstance = new Sequelize('', {
+const sequelizeInstance = new Sequelize(DB_URI, {
     dialect: 'postgres',
     logging: true
 })
+
 
 module.exports = sequelizeInstance;
