baca8374f27aecc2b7a49921bd5ad2b7bbf6a3a1
Yosber Guerrero <yosbergc@gmail.com>
2024-11-04 15:36:10 -0400

    Added post router

---
+++ routes/post.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const Post = require('../schemas/Post')
+
+router.post('/', async (req, res) => {
+    const { title, body, userId, blogId } = req.body
+
+    if(!title || !body || !userId || !blogId ) {
+        res.status(400).send('Bad request')
+    }
+
+    try {
+        const newPost = await Post.create({
+            title: title,
+            body: body,
+            userId: userId,
+            blogId: blogId
+        })
+    
+        res.json(newPost)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router;
