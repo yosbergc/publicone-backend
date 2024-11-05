cacc3a7e9a55cf850ec53ea57157ab27b2778aa7
Yosber Guerrero <yosbergc@gmail.com>
2024-11-04 23:40:18 -0400

    Added middleware to check if user is logged in private routes

--- middlewares/checkUserLogged.js
+++ middlewares/checkUserLogged.js
@@ -1,3 +1,20 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 module.exports = (req, res, next) => {
-    
+    const authorization = req.get('authorization')
+
+    if (!authorization || !authorization.toLowerCase().startsWith('bearer')) {
+        return res.status(400).send('Bad request')
+    }
+
+    const token = token.slice(7, authorization.length)
+
+    try {
+        const isCorrectToken = jwt.verify(token, process.env.PRIVATE_KEY)
+
+        req.userId = isCorrectToken.id
+        next()
+    } catch {
+        return res.status(400).send('There is a problem with your token, try to relogging')
+    }
 }
